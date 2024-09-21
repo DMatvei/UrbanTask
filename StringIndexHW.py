b09dcdccd64a6e7c64e047f32b7eab7924649e65
DMatvei <matvei.dranitsyn@yandex.ru>
2024-09-21 19:35:09 +1000

    first hw

---
+++ StringIndexHW.py
@@ -0,0 +1,10 @@
+complete_HW = 12
+spent_time = 1.5 #hour
+name_course = 'Python'
+time_on_task = spent_time / complete_HW
+print("Курс: " + name_course + f", Всего задач: {complete_HW}"
+      + f', затрачено часов: {spent_time}, среднее время выполнения: {time_on_task}' )
+
+# принт по заданию
+print("Курс:", name_course, ", Всего задач:", complete_HW, ', затрачено часов:', spent_time,
+      ', среднее время выполнения:', time_on_task)
