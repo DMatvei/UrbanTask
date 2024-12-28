219e284cad4882995ed58e58c468b4ea9ffc1734
DMatvei <matvei.dranitsyn@yandex.ru>
2024-12-28 16:11:02 +1000

    HW 4_1

---
+++ Module_4/Task_1/fake_math.py
@@ -0,0 +1,12 @@
+
+def divide(first, second):
+    if second == 0:
+        return  "Error"
+    return first / second
+
+
+
+
+
+if __name__ =="__main__":
+    print('Launch fake_math')
---
+++ Module_4/Task_1/true_math.py
@@ -0,0 +1,13 @@
+from math import inf
+
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    return first / second
+
+
+
+
+if __name__ =="__main__":
+    print('Launch true_math')
